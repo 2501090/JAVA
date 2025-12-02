feb46055e9e248d70d6e6f1ffc7b9ddecdec7bf2
baejunghwan <hwanarchive@gmail.com>
2025-12-02 22:57:48 +0900

    12주차_과제

--- week12/Report12-2_B/src/Compute.java
+++ week12/Report12-2_B/src/Compute.java
@@ -2,7 +2,7 @@ public class Compute {
     public static int leapYear(int year) {
         boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 
-        // 윤년이면 1, 평년이면 0 반환
+        // 윤년이면 1, 평년이면 0 반환함
         return isLeap ? 1 : 0;
     }
 }
